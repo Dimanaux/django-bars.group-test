ad961bf5f6a4918103465adc3c6aa72e73d3d5f1
cosmoskey <barskoidim@gmail.com>
2019-08-15 19:24:01 +0300

    fix errors in config

--- recruitment/admin.py
+++ recruitment/admin.py
@@ -2,9 +2,7 @@ from django.contrib import admin
 
 from .models import *
 
-admin.register(
-    Recruit,
-    Sith,
-    Planet,
-    ShadowHand,
-)
+admin.site.register(Recruit)
+admin.site.register(Sith)
+admin.site.register(Planet)
+admin.site.register(ShadowHand)
--- sith_order/settings.py
+++ sith_order/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC+3'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
