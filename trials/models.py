2896c867af8e04afeef2c63cb9be4707af6b7542
cosmoskey <barskoidim@gmail.com>
2019-08-15 19:24:37 +0300

    add meta information to trials.models

--- trials/models.py
+++ trials/models.py
@@ -10,8 +10,14 @@ class TrialQuestion(models.Model):
     question = models.TextField()
     trial = models.ForeignKey('Trial', on_delete=models.CASCADE)
 
+    class Meta:
+        ordering = ('order',)
+
 
 class RecruitAnswer(models.Model):
     recruit = models.ForeignKey('recruitment.Recruit', on_delete=models.CASCADE)
     question = models.ForeignKey('TrialQuestion', on_delete=models.CASCADE)
     answer = models.BooleanField()
+
+    class Meta:
+        ordering = ('question__order',)
