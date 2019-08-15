1619e6cd7513c3a5002227f804b1e48365fa8f0a
cosmoskey <barskoidim@gmail.com>
2019-08-15 20:37:10 +0300

    urls for trials

--- trials/urls.py
+++ trials/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
+    path('<int:trial_id>/questions/<int:question_order>', views.question, name='question'),
+    path('<int:trial_id>', views.trial, name='trial'),
+    path('<int:trial_id>/complete', views.complete, name='complete'),
 ]
