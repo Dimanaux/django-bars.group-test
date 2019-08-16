43391d302d008964cdd29ed4ff8d957b2c1334cf
cosmoskey <barskoidim@gmail.com>
2019-08-16 13:10:24 +0300

    static files root

--- sith_order/settings.py
+++ sith_order/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    # 'django.contrib.staticfiles',
     'recruitment',
     'trials',
 ]
@@ -113,6 +113,12 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = '/'
 
 # Email settings
 
